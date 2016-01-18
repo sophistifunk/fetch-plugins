57300cfa4b7b49025107a6dcd0d30da636a410e8
Josh McDonald <josh@joshmcdonald.info>
2016-01-18 16:59:05 +1100

    Fetch descriptions, urls, etc as well as install counts

--- get-metadata.js
+++ get-metadata.js
@@ -1,4 +1,3 @@
-
 var Promise = require("bluebird");
 var request = require("request-promise");
 var fs = Promise.promisifyAll(require("fs"));
@@ -14,45 +13,55 @@ var ofileUpdates = "update-center-response.json";
 // --
 
 var usageUrl = "http://stats.jenkins-ci.org/plugin-installation-trend/latestNumbers.json";
-var ofileUsage = "update-center-response.json";
+var ofileUsage = "usage-stats-response.json";
 
 //---[ Helpers ]--------------------------------------------------------------------------------------------------------
 
 var parseJSON = JSON.parse.bind(JSON);
 
+function get(url) {
+    console.log(url, "loading...");
+    return request.get(url)
+        .then(function (result) {
+            console.log(url, result.length, "chars returned.");
+            return result;
+        })
+}
+
 //---[ Main ]-----------------------------------------------------------------------------------------------------------
 
-console.log('Loading from "' + updateUrl + '".');
+console.log("Getting plugin metadata from jenkins-ci.org");
 
 if (!String.prototype.startsWith) throw new Error("Need String.prototype.startsWith, suggest Node 4+");
 
-request.get(updateUrl)
-	.then(function(result){
-		console.log("Got", result.length, "chars back");
-		if (!result.startsWith(prefix) || !result.endsWith(suffix)) {
-			throw new Error("Unexpected result format :(");
-		}
-		result = result.substr(prefix.length, result.length - prefix.length - suffix.length);
-		return result;
-	})
-	.then(parseJSON)
-	.then(function(obj) {
-		console.log("Top-level object has", Object.keys(obj));
-		obj.__comment = "Fetched from " + updateUrl + " on " + new Date();
-		return JSON.stringify(obj,null,4);	
-	})
-	.then(function(niceJSON){
-		return fs.writeFile(ofileUpdates, niceJSON, "utf8");
-	})
-	.then(function(){
-		console.log("Wrote", ofileUpdates);
-		console.log("Loading from \"" + usageUrl + "\"...");
-		return request.get(usageUrl);
-	})
-	.then(parseJSON)
-	.then(function(usageData) {
-		
-	})
-	;
+get(updateUrl)
+    .then(function (result) {
+        if (!result.startsWith(prefix) || !result.endsWith(suffix)) {
+            throw new Error("Unexpected result format :(");
+        }
+        result = result.substr(prefix.length, result.length - prefix.length - suffix.length);
+        return result;
+    })
+    .then(parseJSON)
+    .then(function (obj) {
+        console.log("Top-level object has", Object.keys(obj));
+        obj.__comment = "Fetched from " + updateUrl + " on " + new Date();
+        return JSON.stringify(obj, null, 4);
+    })
+    .then(function (niceJSON) {
+        return fs.writeFile(ofileUpdates, niceJSON, "utf8");
+    })
+    .then(function () {
+        console.log("Wrote", ofileUpdates);
+        return get(usageUrl);
+    })
+    .then(parseJSON)
+    .then(function (usageData) {
+        return fs.writeFile(ofileUsage, JSON.stringify(usageData, null, 4), "utf8");
+    })
+    .then(function () {
+        console.log("Wrote", ofileUsage);
+    })
+;
 
 
